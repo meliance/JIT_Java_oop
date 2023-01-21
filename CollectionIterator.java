d1de05ad13ede2e139434d37fbb9d3c41576d0cd
Melaku <111016250+meliance@users.noreply.github.com>
2023-01-21 08:49:44 +0300

    Add files via upload

---
+++ CollectionIterator.java
@@ -0,0 +1,48 @@
+
+package com.mycompany.collectioniterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+public class CollectionIterator {
+
+	public static void main(String[] args)
+	{ 
+		// Creating an ArrayList class object
+		// Declaring object of integer type
+		ArrayList<Integer> al = new ArrayList();//<Integer>();
+
+		// Iterating over the List
+		for (int i = 0; i < 10; i++)
+			al.add(i);              
+		// Printing the elements in the List
+		System.out.println(al);
+              
+		// At the beginning itr(cursor) will point to
+		// index just before the first element in al
+		Iterator<Integer> itr = al.iterator();
+
+		// Checking the next element where
+		// condition holds true till there is single element
+		// in the List using hasnext() method
+                System.out.println(itr.next());
+                System.out.println(itr.hasNext());
+		while (itr.hasNext()) {
+			// Moving cursor to next element
+			int i = itr.next();
+
+			// Getting elements one by one
+			System.out.print(i + " ");
+
+			// Removing odd elements
+			if (i % 2 != 0)
+				itr.remove();
+		}
+
+		// Command for next line
+		System.out.println();
+
+		// Printing the elements inside the object
+		System.out.println(al);
+                System.out.println(itr.hasNext());
+	}
+}
